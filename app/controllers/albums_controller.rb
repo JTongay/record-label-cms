86b76383222b88b77ee8ab7101c1a9aaa178d5cd
Joseph Tongay <dirk.tongay@gmail.com>
2017-07-25 20:38:17 -0500

    added more stuff to controller and form

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,5 +1,10 @@
 class AlbumsController < ApplicationController
   def new
     @album = Album.new
+    @bands = []
+    Band.all.each do |b|
+      @bands.push(b.band_name)
+    end
+    puts @bands
   end
 end
