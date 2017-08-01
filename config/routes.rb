9b278846c16aa5090db25b5ba98bcb67e8768e82
Joseph Tongay <dirk.tongay@gmail.com>
2017-07-31 21:22:03 -0500

    yo i gotta do a bit of magic to add data for 3 models. i wonder if theres a better solution

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :bands, only: [ :new, :create, :edit, :index ]
-  resources :albums, only: [ :new, :create, :edit ]
+
   root to: 'welcome#index'
+
+  resources :bands, only: [ :new, :create, :edit, :index, :show ] do
+    resources :albums, only: [ :new, :create, :edit, :show ]
+  end
+
+  resources :albums, only: [ :new, :create, :edit, :show ]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
