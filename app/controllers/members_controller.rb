803223241105ae2dda98b45cf060af9c5254aec0
Joseph Tongay <dirk.tongay@gmail.com>
2017-09-17 16:44:45 -0500

    start some authentication

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,17 @@
 class MembersController < ApplicationController
+    def index
+
+    end
+
+    def create
+
+    end 
+
+    def show
+
+    end
+
+    def destroy
+
+    end     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
     resources :albums, only: [ :new, :create, :edit, :show ]
   end
 
+  resources :members
+
   resources :albums, only: [ :new, :create, :edit, :index, :show ]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
