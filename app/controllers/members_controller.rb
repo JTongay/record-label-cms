de09518aa1e8680d0526a7d960d1c74a1a80c2af
Joseph Tongay <dirk.tongay@gmail.com>
2017-09-19 20:57:56 -0500

    that was easy. authentication works when creating an account

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -8,7 +8,14 @@ class MembersController < ApplicationController
     end
 
     def create
-
+        if params[:password] == params[:password_confirmation]
+           @member = Member.create(member_params)
+           if @member.save
+               redirect_to root_url, notice: "#{@member.member_name} was created" 
+           end 
+        else
+            redirect_to new_member_path
+        end
     end 
 
     def show
@@ -18,4 +25,9 @@ class MembersController < ApplicationController
     def destroy
 
     end     
+
+    private
+    def member_params
+        params.require(:member).permit(:member_name, :password, :password_confirmation)
+    end    
 end
