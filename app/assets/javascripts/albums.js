81d97747dd94611ae82476ace410dfbdc9e605d7
Joseph Tongay <dirk.tongay@gmail.com>
2018-09-20 15:56:58 -0500

    made a few styling fixes

--- app/assets/javascripts/albums.js
+++ app/assets/javascripts/albums.js
@@ -3,25 +3,21 @@
 
 $(document).ready(function(){
 
-    $('h1').on('click', function(){
-     console.log($(this).text())
-    });
-
 
     //Come back to this. Add an input for adding a song.
     $('#add-song').on('click', function(ev){
         ev.preventDefault();
         var $currentInput = $('.song-title-input').last().clone();
-        var $inputName = $currentInput.attr('name')
-        var $currentIndex = $inputName.split('[')
-        var $changeIndex = $currentIndex[2].split('')
-        $changeIndex[0] = +$changeIndex[0] + 1
-        var $newIndex = $changeIndex.join("")
-        $currentIndex[2] = $newIndex.toString()
-        var $joinNew = $currentIndex.join('[')
-        $currentInput.attr('name', $joinNew)
-        $currentInput.appendTo('#song-title-section')
-        console.log($currentInput.attr('name'))
+        var $inputName = $currentInput.attr('name');
+        var $currentIndex = $inputName.split('[');
+        var $changeIndex = $currentIndex[2].split('');
+        $changeIndex[0] = +$changeIndex[0] + 1;
+        var $newIndex = $changeIndex.join("");
+        $currentIndex[2] = $newIndex.toString();
+        var $joinNew = $currentIndex.join('[');
+        $currentInput.attr('name', $joinNew);
+        $currentInput.appendTo('#song-title-section');
+        console.log($currentInput.attr('name'));
 
         // $('.song-title-input').first().clone().appendTo('#song-title-section')
     })
